bb7045e96f43202849dc81f479fb0fd0a9b41961
Shantanu Gupta <shantanu@viki.com>
2016-03-11 15:04:43 +0800

    Added data.html and indexData.js

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -1,4 +1,5 @@
-var twitterClient = require(process.cwd() + '/lib/twitter');
+var twitterClient = require(process.cwd() + '/lib/twitter'),
+  solrClient = require(process.cwd() + '/lib/solr');
 
 exports.addTwitterHandle = function(req, res, next) {
 
